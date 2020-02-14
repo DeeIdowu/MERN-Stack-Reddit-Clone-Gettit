8c2121471ca97ae2d7e8a6875430485f69cbfc38
Dennis Idowu <deethegreat@Denniss-MacBook-Air.local>
2020-02-13 21:37:55 -0500

    new components, home and registration

---
+++ client/src/components/pages/Home/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Home extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div>
+        <h1 style={{ textAlign: "center" }}>HomePage</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
