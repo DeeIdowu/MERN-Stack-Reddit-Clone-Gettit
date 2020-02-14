30c18652e65ab2be349461051d1d1af3a9d94b32
Dennis Idowu <deethegreat@Denniss-MacBook-Air.local>
2020-02-13 21:57:47 -0500

    install styled-components, edited index.css and produced themeprovider for the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+//styling
+import { ThemeProvider } from "styled-components";
+import theme from "../src/theme";
+//header
+import Header from "../src/containers/Header/Header";
+
+//pages
 import Home from "../src/components/pages/Home/Home";
 import Register from "../src/components/pages/Registration/Register";
 import Login from "../src/components/pages/Login/Login";
@@ -7,13 +14,16 @@ import Login from "../src/components/pages/Login/Login";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/register" component={Register} />
-          <Route path="/login" component={Login} />
-        </Switch>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
+          </Switch>
+        </BrowserRouter>
+      </ThemeProvider>
     </div>
   );
 }
